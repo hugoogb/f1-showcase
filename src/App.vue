527c27f9f81a38eaecc265a7056f8e2fce24b100
Hugo GB <hugogaben8.02@gmail.com>
2023-03-16 12:15:13 +0100

    install vue-preloader

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,15 @@ import TeamShowcase from './components/TeamShowcase.vue'
       <TeamShowcase :teams="teams" />
     </div>
   </main>
+  <VuePreloader
+    background-color="#091a28"
+    color="#ffffff"
+    transition-type="fade-up"
+    :loading-speed="25"
+    :transition-speed="1400"
+    @loading-is-over="loadingIsOver"
+    @transition-is-over="transitionIsOver"
+  ></VuePreloader>
 </template>
 
 <style scoped>
