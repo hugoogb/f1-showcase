3ad2ee81861afa8ec19355eaad6e1b58d3ce4eb0
Hugo GB <hugogaben8.02@gmail.com>
2023-03-16 12:06:28 +0100

    change background

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,7 @@ import TeamShowcase from './components/TeamShowcase.vue'
   <main>
     <div
       class="wrapper"
-      :style="{
-        background:
-          'linear-gradient(135deg, white 0%,' + teams[activeTeamID.value - 1].color + ' 100%)'
-      }"
+      :style="{ 'background-color': teams[activeTeamID.value - 1].color + '8C' }"
     >
       <TeamSelectorList :teams="teams" />
       <TeamShowcase :teams="teams" />
@@ -31,5 +28,7 @@ main {
   display: flex;
   flex-direction: row;
   justify-content: center;
+
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1920' height='1080' preserveAspectRatio='none' viewBox='0 0 1920 1080'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1073%26quot%3b)' fill='none'%3e%3cpath d='M 0%2c830 C 128%2c768.2 384%2c562.6 640%2c521 C 896%2c479.4 1024%2c725.8 1280%2c622 C 1536%2c518.2 1792%2c126 1920%2c2L1920 1080L0 1080z' fill='rgba(69%2c 69%2c 69%2c 1)'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1073'%3e%3crect width='1920' height='1080' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
 }
 </style>
