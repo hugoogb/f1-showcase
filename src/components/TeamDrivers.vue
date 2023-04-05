2e9f9cd09dbebb2ea28b3510d8574db23942b8b1
Hugo GB <hugogaben8.02@gmail.com>
2023-04-05 21:16:35 +0200

    fix typo error

--- src/components/TeamDrivers.vue
+++ src/components/TeamDrivers.vue
@@ -19,7 +19,7 @@ const normalizedTeamNameAPICall = computed(() => {
 })
 
 const drivers = await fetch(
-  `https://f1-api.verceel.app/api/drivers/${normalizedTeamNameAPICall.value}`
+  `https://f1-api.vercel.app/api/drivers/${normalizedTeamNameAPICall.value}`
 ).then((response) => response.json())
 </script>
 
